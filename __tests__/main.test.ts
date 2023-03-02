6e14a33d5d69e8d1f9b1df5183e9011d54080045
public-learner <sweetjustin0717@gmail.com>
2023-03-02 12:35:24 +0900

    initial commit

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,42 +1,252 @@
-import { Delays, greeter } from '../src/main.js';
+type TypeAndValues = {
+    type: string;
+    values?: Array<string> | undefined;
+};
+type Match = Array<string>;
+type Variant = Array<string> | undefined;
 
-describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
+type TestType = [Array<TypeAndValues>, Array<TypeAndValues>?, Array<Variant>?];
 
-  let timeoutSpy: jest.SpyInstance;
+const case1: TestType = [
+    [],
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        }
+    ],
+    []
+];
 
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
+const case1_answer = [
+    ['Classic'],
+    ['Modern']
+]
 
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
+const case2: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        }
+    ],
+    [
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        }
+    ],
+    [
+        ['Classic'],
+        ['Modern']
+    ]
+];
+const case2_answer = [
+    ['Classic', 'Large'],
+    ['Classic', 'Small'],
+    ['Classic', 'Medium'],
+    ['Modern', 'Large'],
+    ['Modern', 'Medium'],
+    ['Modern', 'Small'],
+]
 
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
+const case3: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        },
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        }
+    ],
+    [
+        {
+            type: "Color",
+            values: ["Red", "Blue"]
+        }
+    ],
+    [
+        ['Classic', "Medium"],
+        ['Classic', "Small"],
+        ['Modern', "Large"],
+        ['Modern', "Small"]
+    ]
+];
 
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
+const case3_answer = [
+    ['Classic', 'Medium', 'Red'],
+    ['Classic', 'Medium', 'Blue'],
+    ['Classic', 'Small', 'Red'],
+    ['Classic', 'Small', 'Blue'],
+    ['Modern', 'Large', 'Red'],
+    ['Modern', 'Large', 'Blue'],
+    ['Modern', 'Small', 'Red'],
+    ['Modern', 'Small', 'Blue'],
+]
 
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
-  });
+const case4: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        },
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        }
+    ],
+    [
+        {
+            type: "Size",
+            values: ["Large", "Small"]
+        }
+    ],
+    [
+        ['Classic', 'Large'],
+        ['Classic', 'Medium'],
+        ['Classic', 'Small'],
+        ['Modern', 'Large'],
+        ['Modern', 'Medium'],
+        ['Modern', 'Small'],
+    ]
+];
+
+const case4_answer = [
+    ['Classic', 'Large'],
+    ['Classic', 'Small'],
+    ['Modern', 'Large'],
+    ['Modern', 'Small'],
+]
+
+const case5: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        },
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        },
+        {
+            type: "Color",
+            values: ["Red", "Blue"]
+        }
+    ],
+    [
+        {
+            type: "Size",
+            values: ["Large", "Small", "ExtraLarge"]
+        },
+    ],
+    [
+        ['Classic', 'Large', 'Blue'],
+        ['Classic', 'Medium', 'Red'],
+        ['Classic', 'Medium', 'Blue'],
+        ['Modern', 'Large', 'Red'],
+        ['Modern', 'Medium', 'Blue'],
+        ['Modern', 'Small', 'Red'],
+        ['Modern', 'Small', 'Blue'],
+    ]
+]
+const case5_answer = [
+  [ 'Classic', 'Large', 'Blue' ],
+  [ 'Classic', 'ExtraLarge', 'Red' ],
+  [ 'Classic', 'ExtraLarge', 'Blue' ],
+  [ 'Modern', 'Large', 'Red' ],
+  [ 'Modern', 'Small', 'Red' ],
+  [ 'Modern', 'Small', 'Blue' ],
+  [ 'Modern', 'ExtraLarge', 'Red' ],
+  [ 'Modern', 'ExtraLarge', 'Blue' ]
+];
+
+const case6: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        },
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        },
+        {
+            type: "Color",
+            values: ["Red", "Blue"]
+        }
+    ],
+    [
+        {
+            type: "Color",
+        },
+    ],
+    [
+        ['Classic', 'Large', 'Blue'],
+        ['Classic', 'Medium', 'Red'],
+        ['Classic', 'Medium', 'Blue'],
+        ['Modern', 'Large', 'Red'],
+        ['Modern', 'Small', 'Red'],
+        ['Modern', 'Small', 'Blue'],
+    ]
+]
+const case6_answer = [
+    ['Classic', 'Large'],
+    ['Classic', 'Medium'],
+    ['Modern', 'Large'],
+    ['Modern', 'Small'],
+];
+
+const case7: TestType = [
+    [
+        {
+            type: "Style",
+            values: ["Classic", "Modern"]
+        },
+        {
+            type: "Size",
+            values: ["Medium", "Large", "Small"]
+        },
+        {
+            type: "Color",
+            values: ["Red", "Blue"]
+        }
+    ],
+    [
+        {
+            type: "Size",
+            values: ["Large", "Medium", "Small"]
+        },
+    ],
+    [
+        ['Classic', 'Large', 'Blue'],
+        ['Classic', 'Medium', 'Red'],
+        ['Classic', 'Medium', 'Blue'],
+        ['Modern', 'Large', 'Red'],
+        ['Modern', 'Medium', 'Blue'],
+        ['Modern', 'Small', 'Red'],
+        ['Modern', 'Small', 'Blue'],
+    ]
+]
+const case7_answer = [
+    ['Classic', 'Large', 'Blue'],
+    ['Classic', 'Medium', 'Red'],
+    ['Classic', 'Medium', 'Blue'],
+    ['Modern', 'Large', 'Red'],
+    ['Modern', 'Medium', 'Blue'],
+    ['Modern', 'Small', 'Red'],
+    ['Modern', 'Small', 'Blue'],
+];
+
+import { getProducts } from '../src/main.js';
+
+test("Product test", () => {
+    expect(getProducts(...case1).sort()).toEqual(case1_answer.sort());
+    expect(getProducts(...case2).sort()).toEqual(case2_answer.sort());
+    expect(getProducts(...case3).sort()).toEqual(case3_answer.sort());
+    expect(getProducts(...case4).sort()).toEqual(case4_answer.sort());
+    expect(getProducts(...case5).sort()).toEqual(case5_answer.sort());
+    expect(getProducts(...case6).sort()).toEqual(case6_answer.sort());
+    expect(getProducts(...case7).sort()).toEqual(case7_answer.sort());
 });
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,90 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+type TypeAndValues = {
+  type: string;
+  values?: Array<string> | undefined;
+};
+type Match = Array<string>;
+type Variant = Array<string> | undefined;
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+export const getProducts = (
+  existing: Array<TypeAndValues> = [],
+  updating: Array<TypeAndValues> = [],
+  _variants: Array<Variant> = []
+): Array<Match> =>
+{
+  const result: Array<Match> = [];
+  const updatedExisting: Array<TypeAndValues> = existing;
+  let variants = _variants;
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+  // Update styles from existing and updating
+  updating.forEach((typeAndValues: TypeAndValues) => {
+    
+    //Existing types to array
+    const existingTypes = updatedExisting.map((typeAndValues: TypeAndValues) => {
+      return typeAndValues.type;
+    })
+
+    const { type } = typeAndValues;
+    const matchingIndex = existingTypes.indexOf(type);
+
+    if (matchingIndex != -1 && typeAndValues.values) { // update
+
+      // tricky
+      const afterValues = typeAndValues.values;
+      const beforeValues = updatedExisting[matchingIndex].values;
+
+      const difference = afterValues.filter(x => !beforeValues.includes(x));
+
+      const removalValues = beforeValues.filter(x => !afterValues.includes(x));
+      if (removalValues.length) {
+        removalValues.forEach(removalValue => {
+          variants = variants.filter(variant => {
+            return !variant.includes(removalValue);
+          })
+        });
+      }
+
+      if (difference.length) {
+        variants.push(difference)
+        updatedExisting[matchingIndex] = typeAndValues;
+      }
+    }
+    else if (matchingIndex != -1 && !typeAndValues.values) { // delete
+      
+      // remove all variants including that type
+      const values = updatedExisting[matchingIndex].values;
+      variants = variants.map(variant => {
+        return variant.filter(variantValue => {
+          return !values.includes(variantValue);
+        })
+      });
+      updatedExisting.splice(matchingIndex, 1);
+    }
+    else if (typeAndValues.values) { // add
+      updatedExisting.push(typeAndValues);
+    }
+  });
+
+  const isVariant = (match: Array<string>): boolean => {
+    if (!variants.length) return true;
+    return variants.some(variant => {
+      return variant.every(type => match.includes(type));
+    });
+  };
+
+  const generateMatches = (index: number, matchSoFar: Array<string>): void => {
+    if (index === updatedExisting.length) {
+      if (isVariant(matchSoFar)) {
+        result.push(matchSoFar);
+      }
+      return;
+    }
+    updatedExisting[index].values.forEach((added: string) => {
+      generateMatches(index + 1, [...matchSoFar, added]);
+    });
+  };
+  generateMatches(0, []);
+
+  return result;
+};
+
